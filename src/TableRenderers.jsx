d1457fb19f9e5e6f2dbea61c38c6ba760a784466
Alejandro Cavallo <alejandro.cavallo@gmail.com>
2019-01-04 12:08:33 -0300

    adding a function to calculate how many rows should span a parent cell when a subtotal is rendered

--- src/TableRenderers.jsx
+++ src/TableRenderers.jsx
@@ -2,7 +2,106 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {PivotData} from './Utilities';
 
-// helper function for setting row/col-span in pivotTableRenderer
+/**
+ * Calculates how manny rows should be added to the calculated span for subtotal's parent
+ * @param {Array} arr array of table rows keys
+ * @param {int} pos current possition in arr
+ * @param {int} col column to be compared
+ * @returns {int} number of span to be added to the subtotals parent cell
+ */
+const calculateSutotalsParentspan = function(arr, pos, col) {
+  let count = 0;
+  let maxIterations = arr.length - 1;
+  let groupStart = false;
+
+  let j = col;
+  if (!Array.isArray(arr) || col >= arr[0].length - 1) {
+    return count;
+  }
+
+  let x;
+  if (pos !== 0) {
+    let asc, end;
+    let noDraw = true;
+    for (
+      x = 0, end = col, asc = end >= 0;
+      asc ? x <= end : x >= end;
+      asc ? x++ : x--
+    ) {
+      // detect when the group/subgroup has finished
+      if (arr[pos - 1][x] !== arr[pos][x]) {
+        noDraw = false;
+      }
+    }
+
+    // if we stay in the same subgroup (or there is no grouping), don't continue processing the array
+    if (noDraw) {
+      return 0;
+    }
+  }
+
+  // iterate over the columns except the last one which will be always no grouped
+  while (j < arr[0].length - 1) {
+    let i = pos;
+    groupStart = false;
+    while (i < arr.length - 1 && i < maxIterations) {
+      if (arr[i][j] === arr[i + 1][j]) {
+        groupStart = true;
+      } else if (groupStart) {
+        // store the max iterations only when analyzing the first column
+        if (j === col) {
+          maxIterations = i;
+        } else {
+          // if it's analyzing a subgroup, increment the counter
+          count++;
+          groupStart = false;
+        }
+      }
+      i++;
+    }
+    // if the end of the row has been reached with a last grouping
+    if (j !== col && groupStart) {
+      count++;
+    }
+    j++;
+  }
+
+  return count;
+};
+/**
+ * This function is used to detect if the subtotal row should be rendered. to do that it compares i-1, i, and i+1
+ * @param {Array} arr array of table rows to be analized
+ * @param {int} pos table row to be analyzed
+ * @param {int} col table colum used in the comparison
+ * @returns {boolean} returns true if the subtotal row should be rendered after the possition indicated by pos
+ */
+const renderSubtotal = function(arr, pos, col) {
+  let doRenderSubtotal = true;
+  // do not compare subtotal for the last column due to always will be ungrouped
+  if (!Array.isArray(arr) || col >= arr[0].length - 1) {
+    return false;
+  }
+  // do not render subtotal after the first row
+  if (pos === 0) {
+    return false;
+  }
+
+  doRenderSubtotal &= arr[pos - 1][col] === arr[pos][col];
+  // if the current item is equal to pos - 1, compare the next one to detect if we are at the end of the group
+  if (doRenderSubtotal && pos < arr.length - 1) {
+    doRenderSubtotal &= arr[pos][col] !== arr[pos + 1][col];
+  }
+
+  return doRenderSubtotal;
+};
+
+/**
+ * helper function for setting row/col-span in pivotTableRenderer
+ * @param {Array} arr bi-dimensional array of row/cols elements
+ * @param {int} i arr starting row
+ * @param {int} j end offset in arr row
+ * @returns {int} numbers of rows/cols to span or -1 if the span is not applicable
+ */
 const spanSize = function(arr, i, j) {
   let x;
   if (i !== 0) {
@@ -13,23 +112,30 @@ const spanSize = function(arr, i, j) {
       asc ? x <= end : x >= end;
       asc ? x++ : x--
     ) {
+      // detect when the group/subgroup has finished
       if (arr[i - 1][x] !== arr[i][x]) {
         noDraw = false;
       }
     }
+
+    // if we stay in the same subgroup (or there is no grouping), don't continue processing the array
     if (noDraw) {
       return -1;
     }
   }
+
   let len = 0;
+
   while (i + len < arr.length) {
     let asc1, end1;
     let stop = false;
+
     for (
       x = 0, end1 = j, asc1 = end1 >= 0;
       asc1 ? x <= end1 : x >= end1;
       asc1 ? x++ : x--
     ) {
+      // detect when a group has finished
       if (arr[i][x] !== arr[i + len][x]) {
         stop = true;
       }
@@ -37,8 +143,10 @@ const spanSize = function(arr, i, j) {
     if (stop) {
       break;
     }
+
     len++;
   }
+
   return len;
 };
 
@@ -55,28 +163,29 @@ function redColorScaleGenerator(values) {
 function makeRenderer(opts = {}) {
   class Subtotal extends React.Component {
     render() {
-      if (this.props.row.length < 2) {
-        return null;
-      }
-      if ((this.props.index + 1) % this.props.row.length !== 0) {
-        return null;
-      }
-      return (
-        <tr key={`subtotal${this.props.index}`}>
-          <th
-            className="pvtTotalLabel"
-            colSpan={
-              this.props.rowAttrs.length +
-              (this.props.colAttrs.length === 0 ? 0 : 1)
-            }
-          >
-            Subtotal
-          </th>
-        </tr>
-      );
+      return this.props.rowAttrs.map((txt, col) => {
+        if (!renderSubtotal(this.props.rowKeys, this.props.index, col)) {
+          return null;
+        }
+        return (
+          <tr key={`subtotal${this.props.index}-${col}`}>
+            <th
+              className="pvtTotalLabel"
+              colSpan={
+                this.props.rowAttrs.length -
+                col +
+                (this.props.colAttrs.length === 0 ? 0 : 1)
+              }
+            >
+              Subtotal {txt}
+            </th>
+          </tr>
+        );
+      });
     }
   }
-  class TableRenderer extends React.PureComponent {
+
+  class TableRenderer extends React.Component {
     render() {
       const pivotData = new PivotData(this.props);
       const colAttrs = pivotData.props.cols;
@@ -230,10 +339,14 @@ function makeRenderer(opts = {}) {
               return [
                 <tr key={`rowKeyRow${i}`}>
                   {rowKey.map(function(txt, j) {
-                    const x = spanSize(rowKeys, i, j);
+                    const x =
+                      spanSize(rowKeys, i, j) +
+                      calculateSutotalsParentspan(rowKeys, i, j);
                     if (x === -1) {
                       return null;
                     }
+                    // x += calculateSutotalsParentspan(rowKeys, i, j);
+
                     return (
                       <th
                         key={`rowKeyLabel${i}-${j}`}
@@ -281,6 +394,8 @@ function makeRenderer(opts = {}) {
                   </td>
                 </tr>,
                 <Subtotal
+                  key={i}
+                  rowKeys={rowKeys}
                   index={i}
                   row={rowKey}
                   rowAttrs={rowAttrs}
@@ -288,6 +403,7 @@ function makeRenderer(opts = {}) {
                 />,
               ];
             })}
+
             {opts.subtotals && colAttrs.length > 1 ? (
               <tr>
                 <th
